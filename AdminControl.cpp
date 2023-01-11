f631a9bf036a0de6a8d822be71937d633f4acdac
sjpark <vkeh9656@gmail.com>
2023-01-11 17:36:30 +0900

    초기 커밋.

---
+++ AdminControl.cpp
@@ -0,0 +1,46 @@
+﻿
+// AdminControl.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "AdminControl.h"
+#include "AdminControlDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CAdminControlApp
+
+BEGIN_MESSAGE_MAP(CAdminControlApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CAdminControlApp 생성
+
+CAdminControlApp::CAdminControlApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CAdminControlApp 개체입니다.
+
+CAdminControlApp theApp;
+
+
+// CAdminControlApp 초기화
+
+BOOL CAdminControlApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	CAdminControlDlg dlg;
+	dlg.DoModal();
+	return FALSE;
+}
+
